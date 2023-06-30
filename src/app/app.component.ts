666e07ef24ea7ec57ea185ab098ac99292b66d81
jztimm <jztimm@gmail.com>
2023-06-30 11:44:28 -0400

    Input error and append user works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'teamApp';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = "Name can't be empty!";
+      return;
+    }
+
+    this.errorMessage = '';
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
 }
